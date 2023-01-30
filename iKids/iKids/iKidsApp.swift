f8b0476cba2d41ab29f7e884785ed7a5ce0ad16b
Raneem AlRashoud <439200695@student.ksu.edu.sa>
2023-01-30 18:16:57 +0300

    Set Up Folders Grouping

---
+++ iKids/iKids/iKidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iKidsApp.swift
+//  iKids
+//
+//  Created by Raneem AlRashoud on 30/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct iKidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
