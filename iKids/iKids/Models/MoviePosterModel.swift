bd79315f9355f59b712a38a09a2b8a061a98a756
Raneem AlRushud <59771760+RaneemAlRushud@users.noreply.github.com>
2023-01-31 19:30:31 +0300

    Update data types

--- iKids/iKids/Models/MoviePosterModel.swift
+++ iKids/iKids/Models/MoviePosterModel.swift
@@ -7,16 +7,28 @@
 
 import Foundation
 
-struct MoviePoster: Identifiable {
+class MoviePoster: Deccodable {
     
     let id : String = UUID().uuidString
-    let image: String
-    let name: String
-    let description: String
-    let rate: String
-    let date : String
-    let time : String
-    let isFaved: Bool
+    var image: String?
+    var name: String?
+    var description: String?
+    var rate: String?
+    var date : String
+    var time : String
+    var isFaved: Bool
+    
+    
+    enum MoviePosterCodingKeys: String, CodingKey {
+    //List  cases
+    
+    }
+    
+    required public init (from decoder: Decoder) throws {
+        
+   // self.id = try? values.decodeIfPresent(, forKey: .)
+        
+    }
     
 }
 
