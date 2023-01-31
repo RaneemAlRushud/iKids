3cc0fe3eccad7aad59ac209832fd754ffa2f9a99
Raneem AlRashoud <439200695@student.ksu.edu.sa>
2023-01-31 19:41:18 +0300

    Add MovieDetaliedView.swift as SubView

---
+++ iKids/iKids/Views/SubViews/MovieDetailedView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieDetailedView.swift
+//  iKids
+//
+//  Created by Raneem AlRashoud on 31/01/2023.
+//
+
+import SwiftUI
+
+struct MovieDetailedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieDetailedView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieDetailedView()
+    }
+}
